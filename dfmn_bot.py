01b76de7816ad319239d09ad85c200dfc6ae5620
Javier <javierphon@gmail.com>
2023-04-21 01:55:08 +0800

    Add location getter function

--- dfmn_bot.py
+++ dfmn_bot.py
@@ -22,7 +22,7 @@ def make_decision(message):
 	
 
 @dfmn_bot.message_handler(commands=['coin'])
-def coin_flip(message):
+def flip_coin(message):
 	CoinFlipper.flip(dfmn_bot, message.chat.id)
 	
 
@@ -37,6 +37,19 @@ def generate_random_number(message):
 def send_dice(message):
 	dfmn_bot.send_dice(message.chat.id)
 
+
+@dfmn_bot.message_handler(commands=['location'])
+def get_location(message):
+    sent_message = dfmn_bot.send_message(message.chat.id, "Please share your location.")
+    dfmn_bot.register_next_step_handler(sent_message, handle_location)
+    
+
+def handle_location(message):
+    chat_id = message.chat.id
+    location = message.location
+    latitude = location.latitude
+    longitude = location.longitude
+    dfmn_bot.send_message(chat_id, f"Your location is: ({latitude}, {longitude})")
 		
 
 @dfmn_bot.message_handler(func=lambda m: True)
