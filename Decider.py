068b82f531360bf8172087933bc162066e6c5d16
Javier <javierphon@gmail.com>
2023-04-20 20:53:44 +0800

    Refactor make decision method to mimic real life convo

--- Decider.py
+++ Decider.py
@@ -4,10 +4,7 @@ class Decider:
 
     @staticmethod
     def choose_options(message: str) -> str:
-        choice_str = message[7:].strip()
-        if not choice_str:
-            return "Please provide at least 1 option, or multiple options separated by commas"
-        choices = choice_str.split(",")
+        choices = message.split(",")
         weights = [1 / len(choices)] * len(choices)
         decision = random.choices(choices, weights=weights, k=1)
         return f"The decision is {decision[0].strip()}"
@@ -20,12 +17,7 @@ class Decider:
         return decision[0]
 
     @staticmethod
-    def generate_rand_int(message) -> str:
-        num_str = message[7:].strip()
-        if not num_str:
-            return "Please provide a range separated by commas. Eg: 1, 100"
-        num_range = num_str.split(",")
-        start = int(num_range[0].strip())
-        end = int(num_range[-1].strip())
-        return str(random.randint(start, end))
+    def generate_rand_int(start, stop) -> str:
+        rand_int = random.randint(start, stop)
+        return f"The random number is: {rand_int}"
         
--- dfmn_bot.py
+++ dfmn_bot.py
@@ -17,8 +17,15 @@ def send_welcome(message):
 
 @dfmn_bot.message_handler(commands=['decide'])
 def make_decision(message):
-	reply = Decider.choose_options(message.text)
-	dfmn_bot.reply_to(message, reply)
+	text = "Please input at least 1 option or multiple options separated by commas"
+	sent_message = dfmn_bot.reply_to(message, text)
+	dfmn_bot.register_next_step_handler(sent_message, options_handler)
+	
+
+def options_handler(message):
+	options = message.text
+	decision = Decider.choose_options(options)
+	dfmn_bot.reply_to(message, decision)
 	
 
 @dfmn_bot.message_handler(commands=['coinflip'])
@@ -29,9 +36,40 @@ def coin_flip(message):
 
 @dfmn_bot.message_handler(commands=['number'])
 def generate_random_number(message):
-	reply = Decider.generate_rand_int(message.text)
-	dfmn_bot.reply_to(message, reply)
+	text = 'Enter lower bound'
+	sent_message = dfmn_bot.send_message(message.chat.id, text)
+	dfmn_bot.register_next_step_handler(sent_message, lower_bound_handler)
+		
 
+def lower_bound_handler(message):
+	num_str = message.text.strip()
+	try:
+		lower_bound = int(num_str)
+		text = 'Enter upper bound'
+		sent_message = dfmn_bot.send_message(message.chat.id, text)
+		dfmn_bot.register_next_step_handler(sent_message, upper_bound_handler, lower_bound)
+	except ValueError:
+		text = f'{num_str} is not a valid integer. Please try again'
+		dfmn_bot.send_message(message.chat.id, text)
+	
+
+def upper_bound_handler(message, lower_bound):
+	num_str = message.text.strip()
+	try:
+		upper_bound = int(num_str)
+		rand_int_str = Decider.generate_rand_int(lower_bound, upper_bound)
+		dfmn_bot.send_message(message.chat.id, rand_int_str)
+	except ValueError:
+		text = f'{num_str} is not a valid integer. Please try again'
+		dfmn_bot.send_message(message.chat.id, text)
+		dfmn_bot.register_next_step_handler(message, lower_bound_handler)
+
+
+@dfmn_bot.message_handler(commands=['dice'])
+def send_dice(message):
+	dfmn_bot.send_dice(message.chat.id)
+
+		
 
 @dfmn_bot.message_handler(func=lambda m: True)
 def echo_all(message):
