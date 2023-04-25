0dc630903f2a6ee67ec1ffcff24081530fc08f6f
Javier <javierphon@gmail.com>
2023-04-25 20:37:19 +0800

    Add multi threaded update handling

--- app/dfmn_bot.py
+++ app/dfmn_bot.py
@@ -1,10 +1,13 @@
 import os
 import telebot
+import logging
+from threading import Thread
 from dotenv import load_dotenv
 from app.constants import *
 from app.suggester import Recommender
 from app.random_utils import *
 from flask import Flask, request
+from collections import Queue
 
 app = Flask(__name__)
 
@@ -12,6 +15,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv(BOT_TOKEN)
 WEBHOOK_DOMAIN = os.getenv(WEBHOOK_DOMAIN)
 WEBHOOK_URL = WEBHOOK_DOMAIN + BOT_TOKEN
+update_queue = Queue()
 
 commands = [telebot.types.BotCommand(SUGGEST_COMMAND, SUGGEST_COMMAND_DESC),
 			telebot.types.BotCommand(DECIDE_COMMAND, DECIDE_COMMAND_DESC),
@@ -23,13 +27,6 @@ commands = [telebot.types.BotCommand(SUGGEST_COMMAND, SUGGEST_COMMAND_DESC),
 dfmn_bot = telebot.TeleBot(BOT_TOKEN)
 dfmn_bot.set_my_commands(commands)
 dfmn_bot.set_my_description(BOT_DESC)
-
-
-@app.route('/' + BOT_TOKEN, methods=[POST_REQUEST])
-def handle_updates():
-    update = telebot.types.Update.de_json(request.stream.read().decode())
-    dfmn_bot.process_new_updates([update])
-    return STATUS_OK
 
 
 @dfmn_bot.message_handler(commands=[START_COMMAND])
@@ -61,3 +58,23 @@ def send_dice(message):
 def suggest(message):
 	Recommender(dfmn_bot, message.chat.id).recommend()
 
+
+@app.route('/' + BOT_TOKEN, methods=[POST_REQUEST])
+def receive_updates():
+    update = telebot.types.Update.de_json(request.stream.read().decode())
+    update_queue.put(update)
+    return STATUS_OK
+
+
+def handle_updates(update_queue: Queue):
+	while True:
+		try:
+			update = update_queue.get()
+			dfmn_bot.process_new_updates([update])
+			update_queue.task_done()
+		except Exception as e:
+			logging.error(e)
+
+
+update_thread = Thread(target=handle_updates, args=(update_queue,))
+update_thread.start()
